739dec15cc0b3aad5e2669f46161c4183f9cc5e9
Daniel Cleveland <dpcleveland@gmail.com>
2017-01-09 21:51:38 -0600

    React props, nesting

--- app/index.js
+++ app/index.js
@@ -1,12 +1,50 @@
+var USER_DATA = {
+  name: 'Daniel Cleveland',
+  username: 'dpcleveland',
+  image: 'https://avatars0.githubusercontent.com/u/2079625?v=3&s=460'
+};
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Hello = React.createClass({
-  render: function () {
+var ProfilePic = React.createClass({
+  render: function() {
+    return <img src={this.props.imageURL} style={{height: 100, width: 100}} />
+  }
+});
+
+var ProfileLink = React.createClass({
+  render: function() {
     return (
-      <div>Hello ReactJS Program!</div>
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
     )
   }
 });
 
-ReactDOM.render(<Hello />, document.getElementById('app'));
+var ProfileName = React.createClass({
+  render: function() {
+    return (
+      <div>
+        {this.props.name}
+      </div>
+    )
+  }
+})
+
+var Avatar = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <ProfilePic imageURL={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
+    )
+  }
+});
+
+ReactDOM.render(<Avatar user={USER_DATA} />, document.getElementById('app'));
