e19aa0cd9f56934efc42103941d0f4d74400e8d1
Daniel Cleveland <dpcleveland@gmail.com>
2017-01-14 22:10:43 -0600

    Confirm battle bootstrap framework

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -7,8 +7,28 @@ function puke (object) {
 function ConfirmBattle(props) {
   return (
     props.isLoading === true
-    ? <p>LOADING</p>
-      : <div>CONFIRM BATTLE!: {puke(props)}</div>
+    ? <p>LOADING</p> :
+      <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+        <h1>Confirm Players</h1>
+        <div className="col-sm-8 col-sm-offset-2">
+          <div className="col-sm-6">
+            <p className="lead">Player 1</p>
+              PLAYER 1 INFO
+          </div>
+          <div className="col-sm-6">
+            <p className="lead">Player 2</p>
+              PLAYER 2 INFO
+          </div>
+        </div>
+        <div className="col-sm-8 col-sm-offset-2">
+          <div className="col-sm-12" style={styles.space}>
+            INITIATE BATTLE BUTTON
+          </div>
+          <div className="col-sm-12" style={styles.space}>
+            LINK TO /PLAYERONE
+          </div>
+        </div>
+      </div>
   )
 }
 
